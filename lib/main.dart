24443e72668fed29d072f937c4d4f83f091e98c0
asclis <jessemin.s@gmail.com>
2023-08-11 16:09:17 +0900

    '23.8.11. add location

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class _sea_tide_appState extends State<sea_tide_app> {
   @override
   void initState() {
     super.initState();
+    _requestLocationPermission(context); // 바로 호출
+
   }
 
   String formatLongitude(double longitude) {
@@ -32,20 +34,31 @@ class _sea_tide_appState extends State<sea_tide_app> {
   @override
   Widget build(BuildContext context) {
 
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Sea Tide App'),
         ),
         body: Center(
-          child: ElevatedButton(
-            child: Text('Get Tide Table'),
-            onPressed: () {
-              _requestLocationPermission(context);
-            },
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Latitude: $latitude',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'Longitude: $longitude',
+                  style: TextStyle(fontSize: 20),
+                )
+              ]
+
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 
@@ -68,6 +81,24 @@ class _sea_tide_appState extends State<sea_tide_app> {
       });
     }
   }
+  _getTideTable(context);
+}
+
+void _getTideTable(BuildContext context) async {
+
+  final url = 'https://api.example.com/tide-table?lat=${position.latitude}&lng=${position.longitude}';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    // Parse the tide table data from the response and display it in the app.
+    // You may need to use a JSON decoding library like the built-in `jsonDecode()` function or the `dart:convert` package.
+  } else {
+    // Handle the case when the API request fails.
+  }
+
+  // Use the position object to fetch the tide table data.
+}
+
 
 }
 
